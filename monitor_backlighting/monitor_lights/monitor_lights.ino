4465b214b47b8799e05b9c2f06ac96b35f197326
Jacek Plonowski <jplonowski@ucdavis.edu>
2019-10-21 19:13:38 -0700

    updated led brightness

--- monitor_backlighting/monitor_lights/monitor_lights.ino
+++ monitor_backlighting/monitor_lights/monitor_lights.ino
@@ -1,9 +1,6 @@
 #include <FastLED.h>
 #include <pixeltypes.h>
 
-// monitor backlighting using ws2812b led's
-// hue controlled by potentiometer
-
 #define NUM_LEDS 56
 #define DATA_PIN 5
 
@@ -18,6 +15,6 @@ FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 
 void loop() { 
 int hueValue = map(analogRead(potInput), 0, 1023, 0, 255);
-FastLED.showColor(CHSV(hueValue, 255, 255)); 
-delay(10);
+FastLED.showColor(CHSV(hueValue, 255, 191)); 
+delay(25);
 }
